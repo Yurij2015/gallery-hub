d75b87df3a066f0f3614097571f32567bdd255fd
Yurii M <mokruy9999@gmail.com>
2025-01-06 21:08:26 +0100

    fix date format

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,3 +1,6 @@
+@php
+use Carbon\Carbon;
+@endphp
 @extends('main')
 @section('content')
     <div
@@ -56,7 +59,8 @@
                             <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">
                                 {{ __('message.editProject') }}
                             </h2>
-                            <form action="/" method="POST" enctype="multipart/form-data">
+                            <form action="{{ route('projects.update', $project->id) }}" method="POST" enctype="multipart/form-data">
+                                @method('put')
                                 @csrf
                                 <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                                     <div class="sm:col-span-2">
@@ -81,7 +85,7 @@
                                             </div>
                                             <input datepicker id="date-datepicker" type="text" name="date"
                                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                   value="{{ $project->date }}">
+                                                   value="{{ Carbon::parse($project->date)->format('d/m/Y') }}">
                                         </div>
                                     </div>
                                     <div class="w-full">
@@ -102,7 +106,7 @@
                                             <input datepicker id="expiration-date-datepicker" type="text"
                                                    name="expiration_date"
                                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                   value="{{ $project->expiration_date }}">
+                                                   value="{{ Carbon::parse($project->expiration_date)->format('d/m/Y') }}">
                                         </div>
                                     </div>
                                     <div class="sm:col-span-2">
@@ -126,7 +130,7 @@
                                         >
                                     </div>
                                 </div>
-                                <button type="submit" disabled
+                                <button type="submit"
                                         class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
                                     {{ __('message.editProject') }}
                                 </button>
