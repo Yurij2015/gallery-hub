e1e8e67cb285118a126a036acc45bfdf2e393406
Yurii M <mokruy9999@gmail.com>
2025-01-06 21:17:51 +0100

    update project

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -17,6 +17,18 @@
                                 {{ __('message.home')}}
                             </a>
                         </li>
+                        <li class="inline-flex items-center">
+                            <a href="{{ route('projects.index') }}"
+                               class="inline-flex items-center text-gray-700 hover:text-primary-600 dark:text-gray-300 dark:hover:text-white">
+                                <svg class="w-5 h-5 mr-2.5" fill="currentColor" viewBox="0 0 20 20"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                          clip-rule="evenodd"></path>
+                                </svg>
+                                {{ __('message.projects') }}
+                            </a>
+                        </li>
                         <li>
                             <div class="flex items-center">
                                 <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20"
