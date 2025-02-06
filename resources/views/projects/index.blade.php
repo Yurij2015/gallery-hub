386680cabd0e21e2d7119f1aeda37111cac67d07
Yurii M <mokruy9999@gmail.com>
2025-02-07 00:43:31 +0100

    set pointer and link to project row

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -108,7 +108,9 @@
                         @if($projects)
                             @php($i = 1)
                             @foreach($projects as $project)
-                                <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                                <tr class="hover:bg-gray-100 dark:hover:bg-gray-700 project-row cursor-pointer"
+                                data-project-edit-url="{{ route('projects.edit', $project->id) }}"
+                                >
                                     <td class="w-4 p-4">
                                         <div class="flex items-center">
                                             <input id="checkbox-{{ $project->id }}" aria-describedby="checkbox-1"
@@ -248,23 +250,6 @@
                                             {{ __('message.view') }}
                                             <div class="tooltip-arrow" data-popper-arrow></div>
                                         </div>
-                                        <a href="{{ route('projects.edit', $project->id) }}"
-                                           data-tooltip-target="tooltip-edit"
-                                           class="inline-flex items-center px-3 py-2 text-xs font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-primary-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-                                            <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
-                                                 xmlns="http://www.w3.org/2000/svg">
-                                                <path
-                                                    d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
-                                                <path fill-rule="evenodd"
-                                                      d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-                                                      clip-rule="evenodd"></path>
-                                            </svg>
-                                        </a>
-                                        <div id="tooltip-edit" role="tooltip"
-                                             class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-xs opacity-0 tooltip dark:bg-gray-800">
-                                            {{ __('message.edit') }}
-                                            <div class="tooltip-arrow" data-popper-arrow></div>
-                                        </div>
                                         <button type="button"
                                                 data-modal-target="delete-project-modal-{{ $project->id }}"
                                                 data-modal-toggle="delete-project-modal-{{ $project->id }}"
@@ -365,6 +350,14 @@
 @push('scripts')
     <script>
         document.addEventListener('DOMContentLoaded', function () {
+            const projectRows = document.querySelectorAll('.project-row');
+            projectRows.forEach(row => {
+                row.addEventListener('click', function () {
+                    window.location.href = row.getAttribute('data-project-edit-url');
+                });
+            });
+
+
             const copyLinks = document.querySelectorAll('.copyLink');
 
             copyLinks.forEach(link => {
